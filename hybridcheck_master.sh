8a34271f31d5bfe99e2800e9616b3c08b8fa7ca3
jelmerp <jelmerpoelstra@gmail.com>
2018-04-25 20:09:26 -0400

    Delete hybridcheck_master.sh

--- hybridcheck_master.sh
+++
@@ -1,5 +0,0 @@
-###### RUN FOR PRE-DEFINED BLOCKS #####
-module load r
-FILE_ID=EjaC.Dstat.DP5.GQ20.MAXMISS0.5.MAF0.01
-BLOCK_P=0.001
-Rscript scripts/windowstats/fd_process3_runHC.R $FILE_ID $BLOCK_P > hybridcheck_knownblocks.txt
